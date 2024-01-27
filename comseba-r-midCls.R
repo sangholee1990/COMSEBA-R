0edbb91685e0a8317ba6328d3cc2b15a90a8eda2
Sang-Ho Lee <sangho.lee.1990@gmail.com>
2024-01-27 18:40:26 +0900

    c

--- comseba-r-midCls.R
+++ comseba-r-midCls.R
@@ -38,6 +38,7 @@ max(5, 3)
 (3 + 5 * 6) / 7
 (7 - 4) * 3
 
+
 # ========================================================
 # 2024.01.20
 # ========================================================
@@ -150,3 +151,168 @@ GNP[c(1)]
 # GNP["Korea"]
 GNP[c("Korea")]
 
+
+# ========================================================
+# 2024.01.27
+# ========================================================
+# 벡터에 대한 산술 연산
+d = c(1, 4, 3, 7, 8)
+d
+
+2 * d
+d - 5
+
+# 우선순위를 통해 괄호 필요
+(3 * d) + 4
+
+# 두 벡터의 연결
+x = c(1, 2, 3)
+y = c(4, 5)
+
+c(x, y)
+z = c(x, y)
+z
+
+# 두 벡터의 합
+x = c(1, 2, 3)
+y = c(4, 5, 6)
+
+z = x + y
+z
+
+# 벡터에 적용 가능한 함수
+d = 1:10
+d
+
+# 합계
+sum(d)
+
+# 개수 
+length(d)
+
+# 최대값
+max(d)
+
+# 최소값
+min(d)
+
+# 오름차순 정렬
+sort(d)
+
+# 내림차순 정렬
+sort(d, decreasing = TRUE)
+
+# 평균 계산
+v2 = sum(d) / length(d)
+v2
+
+v3 = mean(d)
+v3
+
+# 정석
+sort(x = d, decreasing = TRUE)
+
+# 축약
+sort(d, TRUE)
+
+# 축약2
+sort(d, T)
+
+# 연습4
+d1 = 1:50
+d2 = 51:100
+
+d1
+d2
+
+d1 + d2
+d2 - d1
+d1 * d2
+d2 / d1
+
+sum(d1)
+sum(d2)
+
+sum(d1, d2)
+
+max(d2)
+min(d2)
+
+mean(d2) - mean(d1)
+
+sort(d1, decreasing = TRUE)
+
+# sort(d1, decreasing = TRUE)[1:10]
+# sort(d2, decreasing = TRUE)[1:10]
+z = c(
+  sort(d1, decreasing = TRUE)[1:10]
+  , sort(d2, decreasing = TRUE)[1:10]
+)
+z
+
+# 벡터에서 특정 값 추출
+d = 1:9
+
+# 5 이상
+isFlag = (d >= 5)
+isFlag
+
+# 값 추출 (조건: 5 이상)
+# 5 6 7 8 9
+# d[c(5, 6, 7, 8)]
+d[isFlag]
+
+# 총계/합계 (조건: 5 이상)
+sum(d[isFlag])
+
+# 조건 2개 이상 (조건: 5~7 이하)
+isFlag = ((d >= 5) & (d <= 7))
+isFlag
+
+d[isFlag]
+sum(d[isFlag])
+
+# 조건 2개 이상 (조건: 3 미만, 7 이상)
+isFlag = ((d < 3) | (d >= 7))
+isFlag
+
+d[isFlag]
+sum(d[isFlag])
+
+# 연습5
+v1 = 51:90
+
+isFlag = (v1 < 60)
+v1[isFlag]
+
+isFlag = (v1 < 70)
+v1[isFlag]
+length(v1[isFlag])
+
+isFlag = (v1 > 65)
+v1[isFlag]
+sum(v1[isFlag])
+
+
+isFlag = ((v1 > 60) & (v1 < 73))
+isFlag
+v1[isFlag]
+
+isFlag = ((v1 > 80) | (v1 < 65))
+isFlag
+v1[isFlag]
+
+# 도움말
+help(sum)
+
+# 도움말 축약
+?sum
+
+# sum(1:5)
+# sum(1, 2, 3, 4, 5)
+
+# 매트릭스 생성
+z = matrix(1:20, nrow=4, ncol = 5)
+
+# 4 x 5 행렬
+z
